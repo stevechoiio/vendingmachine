5da77cdecb12e275f63aaaffd053dd68bfc7b048
Steve Choi <stevechoi93@gmail.com>
2019-02-24 14:58:04 -0800

    added testing for item inventory and coin inventory

--- src/itemStock.js
+++ src/itemStock.js
@@ -1,13 +1,53 @@
 const itemStock = {
   A1: {
-    name: "Coca-Cola",
+    name: "Cola-Coca",
     price: 2.75,
     stock: 0
   },
   A2: {
-    name: "Diet Coca-Cola",
+    name: "Fat Coca-Cola",
     price: 2.45,
     stock: 32
+  },
+  A3: {
+    name: "Pepsid",
+    price: 13232123123123123123123123123.12,
+    stock: 0
+  },
+  B1: {
+    name: "Suprite",
+    price: 2.45,
+    stock: 1
+  },
+  B2: {
+    name: "Water",
+    price: 1.5,
+    stock: 32
+  },
+  B3: {
+    name: "Dr.Qepper",
+    price: 2.0,
+    stock: 1
+  },
+  B4: {
+    name: "America Dry",
+    price: 123.45,
+    stock: 999
+  },
+  C1: {
+    name: "Iced Tee",
+    price: 4.45,
+    stock: 5
+  },
+  C2: {
+    name: "Parking Water",
+    price: 2.05,
+    stock: 3
+  },
+  C3: {
+    name: "Lemonaid",
+    price: 1.45,
+    stock: 0
   }
 };
 
