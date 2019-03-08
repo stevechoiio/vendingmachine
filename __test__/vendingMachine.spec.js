0921fdda079f3e30652a1a9c01affa524fd7b75a
Steve Choi <stevechoi93@gmail.com>
2019-03-08 14:14:07 -0800

    updated wording on test cases and new screenshot added

--- __test__/vendingMachine.spec.js
+++ __test__/vendingMachine.spec.js
@@ -4,10 +4,9 @@ const coinStock = require("../src/coinStock");
 
 describe("Vending Machine", () => {
   const DefaultVendingMachine = new VendingMachine(itemStock, coinStock);
-
-  describe("Buying product", () => {
-    describe("invalid purchase", () => {
-      it("should give a error message when user inputs invalid itemcode", () => {
+  describe("When user is buying a product,", () => {
+    describe("user can't make a purchase", () => {
+      it("should give an error message when user inputs invalid itemcode", () => {
         expect(DefaultVendingMachine.buy("A11", 5)).toEqual(
           "Invalid item code"
         );
@@ -26,7 +25,7 @@ describe("Vending Machine", () => {
         );
       });
     });
-    describe("valid", () => {
+    describe("user can make a purchase, and it ", () => {
       it("should dispense with no change when user provides exact change", () => {
         expect(DefaultVendingMachine.buy("A2", 2.45)).toEqual(
           "Dispending Fat Coca-Cola"
@@ -44,8 +43,8 @@ describe("Vending Machine", () => {
       });
     });
   });
-  describe("Restocking coin", () => {
-    describe("can't restock, and shouldn't change the quantity because", () => {
+  describe("When admin attemps to restock item", () => {
+    describe("admin can't restock, and shouldn't change the quantity", () => {
       it("should be return an error message when restocking invalid item ", () => {
         expect(DefaultVendingMachine.restockCoin("TOOOOnies", 321)).toEqual(
           "Machine only accepts toonies, loonies, quarters, dimes and nickels"
@@ -64,15 +63,15 @@ describe("Vending Machine", () => {
       });
     });
   });
-  describe("Restocking items", () => {
-    describe("can't restock item", () => {
+  describe("When admin is attempting to restock the item", () => {
+    describe("admin incan't restock item because", () => {
       it("should be invalid item code", () => {
         expect(DefaultVendingMachine.restockItem("A1231", 32)).toEqual(
           "Invalid itemcode"
         );
       });
     });
-    describe("restock item", () => {
+    describe("When restocking item inventory", () => {
       it("should add stock and return message", () => {
         expect(DefaultVendingMachine.restockItem("A1", 1)).toEqual(
           "Restocked 1 Cola-Coca"
@@ -83,13 +82,13 @@ describe("Vending Machine", () => {
       });
     });
   });
-  describe("Printing Inventory", () => {
-    it("should print the item stock", () => {
+  describe("When admin wants to display the stock of the vending machine", () => {
+    it("item, it should print the item stock", () => {
       expect(DefaultVendingMachine.itemInventory()).toEqual(
         "Item Inventory: Cola-Coca for $2.75, and there are  1 in stock, Fat Coca-Cola for $2.45, and there are  32 in stock, Pepsid for $1.3232123123123122e+28, and there are  0 in stock, Suprite for $2.45, and there are  1 in stock, Water for $1.5, and there are  32 in stock, Dr.Qepper for $2, and there are  1 in stock, America Dry for $123.45, and there are  999 in stock, Iced Tee for $4.45, and there are  5 in stock, Parking Water for $2.05, and there are  3 in stock, Lemonaid for $1.45, and there are  0 in stock"
       );
     });
-    it("should print the coin stock", () => {
+    it("coin, it should print the coin stock", () => {
       expect(DefaultVendingMachine.coinInventory()).toEqual(
         "Coin Inventory: 10 toonies, 23 loonies, 151 quarters, 10 dimes, 12 nickels"
       );
